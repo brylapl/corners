685324d5e48f37c3a04d326dec40cb70c775b386
brylapl <92885606+brylapl@users.noreply.github.com>
2024-11-01 12:50:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import streamlit as st
 
-# Ustawienia tytułu aplikacji
+# Funkcja do przekierowania
+def redirect_to(url):
+    # Kod HTML i JavaScript do wykonania przekierowania
+    html = f"""
+    <script>
+        window.location.href = "{url}";
+    </script>
+    """
+    # Wyświetlenie komponentu HTML w Streamlit
+    st.components.v1.html(html)
 
-# HTML do wyświetlenia
-html_code = """
-
-"""
-
-# Wyświetlenie kodu HTML w Streamlit
-st.markdown(html_code, unsafe_allow_html=True)
+# Przykładowe użycie - przekierowanie do innego adresu
+if st.button('Przekierowanie'):
+    redirect_to('https://brylapl.github.io/corner/')   # Podaj adres URL, na który chcesz przekierować
