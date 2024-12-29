a2048851146c7382066a0e804e0a41d63aa5af11
brylapl <92885606+brylapl@users.noreply.github.com>
2024-12-29 10:06:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,3 @@
 import streamlit as st
 
-# Tytuł aplikacji
-
-# Adres URL do otwarcia
-url = "https://brylapl.github.io/corner/"
-
-# Tworzenie przycisku, który otworzy nowy adres w nowym oknie
-if st.button('Otwórz nowy adres'):
-    js = f"""
-    <script>
-        window.open('{url}', '_blank');
-    </script>
-    """
-    st.markdown(js, unsafe_allow_html=True)
+st.title("APP TEST")
