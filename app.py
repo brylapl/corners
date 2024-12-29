f2f1c564fb726989b874b94a0bee01fb30b3ec08
brylapl <92885606+brylapl@users.noreply.github.com>
2024-12-29 10:35:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,26 @@
 import streamlit as st
+import psycopg2
 
-st.title("APP TEST")
+# Wczytanie sekretnych danych z secrets.toml
+db_config = st.secrets["postgresql"]
+
+# Połączenie z bazą danych
+conn = psycopg2.connect(
+    dbname=db_config["dbname"],
+    user=db_config["user"],
+    password=db_config["password"],
+    host=db_config["host"],
+    port=db_config["port"]
+)
+
+# Przykład używania połączenia
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM upcoming_match;")
+rows = cursor.fetchall()
+
+for row in rows:
+    st.write(row)
+
+# Zamknięcie połączenia
+cursor.close()
+conn.close()
